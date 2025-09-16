f050d47a8f972dac694d3b6c18083a490b971b1f
Akhilesh Rangani <akhileshrangani4@gmail.com>
2025-09-16 14:12:11 -0400

    chore: Update Twitter handle in userData

--- lib/data.ts
+++ lib/data.ts
@@ -1,7 +1,7 @@
 export const userData = {
   name: 'Akhilesh Rangani',
   email: 'akhileshrangani4@gmail.com',
-  twitter: 'https://twitter.com/heyavi_',
+  twitter: 'https://twitter.com/akhileshrangani',
   linkedin: 'https://www.linkedin.com/in/akhileshrangani/',
   github: 'https://github.com/akhileshrangani4',
   site: 'https://heyavi.me',
